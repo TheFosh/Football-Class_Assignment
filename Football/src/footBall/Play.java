f2d5bec525d4525a2054648d71da6f359c7147ec
Jake Swanson <jakeswan213@gmail.com>
2023-03-22 16:40:11 -0500

    Made play file

--- Football/src/footBall/Play.java
+++ Football/src/footBall/Play.java
@@ -2,7 +2,27 @@ package footBall;
 
 public class Play {
 	
-	public Play(String des, int yards) {
+	private int yards;
+	private String decription;
+	
+	public Play(String des, int y) {
+		this.yards = y;
+		this.decription = des;
+	}
+	
+	public String toString() {
+		String message = this.decription;
 		
+		if(yards > 0) {
+			message += " a gain " + this.yards + " yards";
+		}
+		else if (yards < 0) {
+			message += " a loss " + this.yards + " yards";
+		}
+		else {
+			message += " no gain " + this.yards;
+		}
+		
+		return message;
 	}
 }
