dcffc89de81faf0193dde9c98b87284427dfb74a
Jake Swanson <jakeswan213@gmail.com>
2023-03-22 22:47:10 -0500

    Made Flag file

--- Football/src/footBall/Flag.java
+++ Football/src/footBall/Flag.java
@@ -1,5 +1,17 @@
 package footBall;
 
-public class Flag {
-
+public class Flag extends Exception {
+	
+	private int yards;
+	
+	public Flag(String p, String s, int y) {
+		
+		super(p + " on the "+ s +" for "+ y +" yards.");
+		this.yards = y;
+	}
+	
+	public int getYards() {
+		return this.yards;
+	}
+	
 }
