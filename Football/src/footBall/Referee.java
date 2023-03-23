619ef930011d636482c23b222d829832a8be0f34
Jake Swanson <jakeswan213@gmail.com>
2023-03-22 22:53:50 -0500

    Made referee file

--- Football/src/footBall/Referee.java
+++ Football/src/footBall/Referee.java
@@ -1,5 +1,29 @@
 package footBall;
 
 public class Referee {
-
+	private int flags;
+	
+	public Referee() {
+		this.flags = 2;
+	}
+	
+	public void watchPlay() throws Flag {
+		
+		int r = (int)(Math.random() * 5 +1);
+		
+		if(this.flags != 0) {
+			
+			//Holding
+			if(r == 1) {
+				this.flags--;
+				throw new Flag("Holding", "Offence", -10);
+			}
+			
+			//Offsides
+			else if (r == 2) {
+				this.flags--;
+				throw new Flag("Offsides", "Defence", 5);
+			}
+		}
+	}
 }
