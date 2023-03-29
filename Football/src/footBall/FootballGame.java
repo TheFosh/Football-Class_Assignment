9b43f7004b56e1086668779ab90da21abf2cb72a
Jake Swanson <jakeswan213@gmail.com>
2023-03-28 22:47:02 -0500

    Created football file

--- Football/src/footBall/FootballGame.java
+++ Football/src/footBall/FootballGame.java
@@ -2,7 +2,31 @@ package footBall;
 
 public class FootballGame {
 	
+	private Referee lineJudge;
+	
+	public FootballGame() {
+		this.lineJudge = new Referee();
+	}
+	
+	public void sertLineJudge(Referee r){
+		this.lineJudge = r;
+	}
+	
+	
+	//Simulates running a play in football
+	//Uses the ref. object to "watch the play" and possibly throw a flag
 	public int runPlay(Play p) {
+		System.out.println(p);
+		
+		try {
+			this.lineJudge.watchPlay();
+		} 
+		catch(Flag f){
+			System.out.println("!!! FLAG ON THE PLAY !!! " + f + " Replay Down.");
+			
+			return f.getYards();
+		}
+		
 		return 0;
 	}
 
